e10e312f69e22da43b273e7e1cfb8b1c89d9496c
Hayder Abu AlHummos <greekrifleopens@gmail.com>
2023-10-04 02:36:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    
+    return gifts;
+}
+
+ const names = (["Charlie", "Samip", "Ali"], "birthday");
+ const event = gifts;
+function writeCards(names, event) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+
+    return thankYouCards;
+}
+
+function countDown() {
+    let count = 10;
+    while (count >= 0) {
+        console.log(count--);
+    }
+}
+countDown();
